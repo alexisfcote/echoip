ca203132c596d38e57a04e757deef26bd5a44a2a
alexisfcote <alexisfcote@gmail.com>
2015-01-09 07:12:41 -0500

    first commit fully functionnal

---
+++ echoip.py
@@ -0,0 +1,23 @@
+from sys import argv
+from flask import Flask, request
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return get_ip()
+
+
+def get_ip():
+    """ This function returns the ip address of the client"""
+    return request.environ['HTTP_REMOTE_ADDR'] 
+
+if __name__ == "__main__":
+    if 'production' in argv:
+        ip = '127.0.0.1'
+        port = 8080
+    else:
+        ip = '0.0.0.0'
+        port = 80
+    app.run(host=ip, port = port, debug = False)
+
